3aaf02e26c3cd4e3faceb7155ec30d219ca8de9f
farooqam <farooq_am@hotmail.com>
2018-08-18 22:23:01 -0700

    Vin value object.

---
+++ DomainModels/Vehicle/Vin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Automagic.DomainModels.Core;
+
+namespace Automagic.DomainModels.Vehicle
+{
+    public class Vin : ValueObject<Vin>
+    {
+        public string Value { get; }
+
+        public Vin(string value)
+        {
+            value.EnsureStringSpecified("Specify a vin.", typeof(Vin));
+
+            Value = value;
+        }
+        protected override HashCode CalculateHashCode()
+        {
+            return new HashCode(Value.ToLowerInvariant().GetHashCode());
+        }
+
+        protected override bool DoEqualityCheck(Vin a, Vin b)
+        {
+            return string.Compare(a.Value, b.Value, StringComparison.InvariantCultureIgnoreCase) == 0;
+        }
+    }
+}
---
+++ Vehicle.Tests/VinTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Automagic.DomainModels.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace Automagic.DomainModels.Vehicle.Tests
+{
+    public class VinTests
+    {
+        [Fact]
+        public void GetTheVin()
+        {
+            // Arrange
+            var expectedValue = "abc123";
+            var vin = new Vin(expectedValue);
+
+            // Act
+            var value = vin.Value;
+
+            // Assert
+            value.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void Vins_AreEqual()
+        {
+            // Arrange
+            var vin1 = new Vin("abc123");
+            var vin2 = new Vin("abc123");
+
+            // Act
+            var areEqual = vin1 == vin2;
+
+            // Assert
+            areEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Vins_AreCaseInsensitive()
+        {
+            // Arrange
+            var vin1 = new Vin("abc123");
+            var vin2 = new Vin("ABC123");
+
+            // Act
+            var areEqual = vin1 == vin2;
+
+            // Assert
+            areEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void HashCodes_AreEqual()
+        {
+            // Arrange
+            var vin1 = new Vin("abc123");
+            var vin2 = new Vin("abc123");
+
+            // Act
+            var hashCodesEqual = vin1.GetHashCode() == vin2.GetHashCode();
+
+            // Assert
+            hashCodesEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void HashCodeCalculation_IsCaseInsensitive()
+        {
+            // Arrange
+            var vin1 = new Vin("abc123");
+            var vin2 = new Vin("ABC123");
+
+            // Act
+            var hashCodesEqual = vin1.GetHashCode() == vin2.GetHashCode();
+
+            // Assert
+            hashCodesEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Vins_AreNotEqual()
+        {
+            // Arrange
+            var vin1 = new Vin("abc123");
+            var vin2 = new Vin("bc123");
+
+            // Act
+            var areEqual = vin1 == vin2;
+
+            // Assert
+            areEqual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Vins_WhenNotEqual_HashCodesNotEqual()
+        {
+            // Arrange
+            var vin1 = new Vin("abc123");
+            var vin2 = new Vin("bc123");
+
+            // Act
+            var areEqual = vin1 == vin2;
+
+            // Assert
+            areEqual.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void WhenVinValueNotSpecified_ThrowException(string value)
+        {
+            // Arrange
+            // ReSharper disable once ObjectCreationAsStatement
+            Action action = () => new Vin(value);
+
+            // Act & Assert
+            var exception = action.Should().Throw<DomainModelException>()
+                .WithMessage("Specify a vin.")
+                .Which;
+
+            exception.Root.Should().Be<Vin>();
+            exception.Child.Should().BeNull();
+        }
+    }
+}
