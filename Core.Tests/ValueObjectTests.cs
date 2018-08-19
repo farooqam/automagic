6991a7677aa974c9820ec710f1bfde5a9c6f6439
farooqam <farooq_am@hotmail.com>
2018-08-18 18:43:35 -0700

    Domain model core - ValueObject base class.

---
+++ Core.Tests/ValueObjectTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Automagic.DomainModels.Core.Tests
+{
+    public class ValueObjectTests
+    {
+        [Fact]
+        public void ValueObjects_AreEqual()
+        {
+            // Arrange
+            var vo1 = new FakeValueObject(1, 2, 3);
+            var vo2 = new FakeValueObject(1, 2, 3);
+
+            // Act
+            var areEqual = vo1 == vo2;
+
+            // Assert
+            areEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValueObjects_WhenEqual_HaveEqualHashCodes()
+        {
+            // Arrange
+            var vo1 = new FakeValueObject(1, 2, 3);
+            var vo2 = new FakeValueObject(1, 2, 3);
+
+            // Act
+            var hashCodesEqual = vo1.GetHashCode() == vo2.GetHashCode();
+
+            // Assert
+            hashCodesEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValueObjects_AreNotEqual()
+        {
+            // Arrange
+            var vo1 = new FakeValueObject(1, 2, 3);
+            var vo2 = new FakeValueObject(2, 2, 3);
+
+            // Act
+            var areEqual = vo1 == vo2;
+
+            // Assert
+            areEqual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ValueObjects_WhenNotEqual_HashCodesNotEqual()
+        {
+            // Arrange
+            var vo1 = new FakeValueObject(1, 2, 3);
+            var vo2 = new FakeValueObject(2, 2, 3);
+
+            // Act
+            var hashCodesEqual = vo1.GetHashCode() == vo2.GetHashCode();
+
+            // Assert
+            hashCodesEqual.Should().BeFalse();
+        }
+    }
+
+    public class FakeValueObject : ValueObject<FakeValueObject>
+    {
+        public int X { get; }
+
+        public int Y { get; }
+
+        public int Z { get; }
+
+        public FakeValueObject(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        protected override HashCode CalculateHashCode()
+        {
+            return new HashCode(X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode());
+        }
+
+        protected override bool DoEqualityCheck(ValueObject<FakeValueObject> a, ValueObject<FakeValueObject> b)
+        {
+            var avo = (FakeValueObject) a;
+            var avb = (FakeValueObject) b;
+
+            return avo.X == avb.X &&
+                   avo.Y == avb.Y &&
+                   avo.Z == avb.Z;
+        }
+    }
+}
---
+++ Core/ValueObject.cs
@@ -0,0 +1,38 @@
+﻿namespace Automagic.DomainModels.Core
+{
+    public abstract class ValueObject<T> where T : class 
+    {
+        protected abstract HashCode CalculateHashCode();
+        protected abstract bool DoEqualityCheck(ValueObject<T> a, ValueObject<T> b);
+
+        public override int GetHashCode()
+        {
+            return CalculateHashCode().Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((ValueObject<T>)obj);
+        }
+
+        public bool Equals(ValueObject<T> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return DoEqualityCheck(this, other);
+        }
+
+        public static bool operator ==(ValueObject<T> a, ValueObject<T> b)
+        {
+            return Equals(a, b);
+        }
+
+        public static bool operator !=(ValueObject<T> a, ValueObject<T> b)
+        {
+            return !(a == b);
+        }
+    }
+}
