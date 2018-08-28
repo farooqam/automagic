928b97a118b5fc7197a7f78e880843169f224ddf
Farooq Mahmud <farooq_am@hotmail.com>
2018-08-28 14:33:46 -0700

    Added more colors.

--- DomainModels/Vehicle/Color.cs
+++ DomainModels/Vehicle/Color.cs
@@ -12,6 +12,18 @@ namespace Automagic.DomainModels.Vehicle
     public static class Colors
     {
         public static Color Unknown = new Color("Unknown");
+        public static Color Beige = new Color("Beige");
         public static Color Black = new Color("Black");
+        public static Color Blue = new Color("Blue");
+        public static Color Brown = new Color("Brown");
+        public static Color Gold = new Color("Gold");
+        public static Color Gray = new Color("Gray");
+        public static Color Green = new Color("Green");
+        public static Color Orange = new Color("Orange");
+        public static Color Purple = new Color("Purple");
+        public static Color Red = new Color("Red");
+        public static Color Silver = new Color("Silver");
+        public static Color White = new Color("White");
+        public static Color Yellow = new Color("Yellow");
     }
 }
